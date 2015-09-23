12d2dcf24fe53c563c43dc4c8d6c00b6d2b48900
Isabella Organ <isabellaorgan@gmail.com>
2015-09-23 12:08:23 -0700

    update simple server code challenge attempting to use design patterns from Eloquent JS - about halfway there. add comments to notate use of http and fs modules, callback, error checking, etc

--- server.js
+++ server.js
@@ -1,24 +1,73 @@
+//Trying updated design patterns via Eloquent JS
+
+
+//http module (provides functionality for running HTTP servers and making HTTP requests) 
 var http = require('http');
+//fs(file system) module (provides functions for working with files and directories)
 var fs = require('fs');
 
-//error checking
-function send404Response(response) {
-	response.writeHead(404, {'Content-Type': 'text/plain'});
-	response.write("Error 404: Page not found");
-	response.end();
+var methods = Object.create(null);
+//the function passed as an argument to createServer is called every time a client tries to connect to the server.
+//request and response represent incoming and outgoing data (I/O)
+http.createServer(function(request, response) {
+//the respond function acts a callback to finish the request
+  function respond(code, body, type) {
+    if (!type) type = "text/plain";
+    response.writeHead(code, {"Content-Type": type});
+    if (body && body.pipe)
+      body.pipe(response);
+    else
+      response.end(body);
+  }
+  if (request.method in methods)
+    methods[request.method](urlToPath(request.index.html),
+                            respond, request);
+  else respond(405, "Method " + request.method +
+              " not allowed.");
+}).listen(9000);
+
+function urlToPath(url) {
+  var path = require("url").parse(url).pathname;
+  return "." + decodeURIComponent(path);
 }
 
+methods.GET = function(path, respond) {
+  fs.stat(path, function(error, stats) {
+    if (error && error.code === "ENOENT")
+      respond(404, "File not found");
+    else if (error)
+      respond(500, error.toString());
+    else if (stats.isDirectory())
+      fs.readdir(path, function(error, files) {
+        if (error)
+          respond(500, error.toString());
+        else
+          respond(200, files.join("\n"));
+      });
+    else
+      respond(200, fs.createReadStream(path),
+              require("mime").lookup(path));
+  });
+};
 
-//request
-function onRequest(request, response) {
+// //error checking
+// function send404Response(response) {
+// 	response.writeHead(404, {'Content-Type': 'text/plain'});
+// 	response.write("Error 404: Page not found");
+// 	response.end();
+// }
 
-	if (request.method === 'GET' && request.url === '/') {
-		response.writeHead(200, {'Content-Type': 'text/html'});
-		fs.createReadStream('./index.html').pipe(response);
-	} else {
-		send404Response(response);
-	}
-}
 
-http.createServer(onRequest).listen(9000);
-console.log("Server is now running.");
+// //request
+// function onRequest(request, response) {
+
+// 	if (request.method === 'GET' && request.url === '/') {
+// 		response.writeHead(200, {'Content-Type': 'text/html'});
+// 		fs.createReadStream('./index.html').pipe(response);
+// 	} else {
+// 		send404Response(response);
+// 	}
+// }
+
+// http.createServer(onRequest).listen(9000);
+// console.log("Server is now running.");
